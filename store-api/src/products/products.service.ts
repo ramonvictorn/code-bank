01897136f6b37c491775d134b5ace385d9290c4d
Ramon Victor do Nascimento <ramonvictorn@gmail.com>
2021-07-19 23:18:02 -0300

    feat: add product structure

--- store-api/src/products/products.service.ts
+++ store-api/src/products/products.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { HttpCode, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityNotFoundError, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-
+import { Product } from './entities/product.entity';
+import { validate as uuidValidate } from 'uuid'
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>
+  ) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const product = this.productRepository.create(createProductDto);
+    return this.productRepository.save(product);
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(idOrSlug: string) {
+    console.log('idOrSlug ??', idOrSlug)
+    const where = uuidValidate(idOrSlug)
+      ? { id: idOrSlug }
+      : { slug: idOrSlug };
+    const product = await this.productRepository.findOne(where);
+    if (!product) {
+      throw new EntityNotFoundError(Product, idOrSlug);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const updateResult = await this.productRepository.update(id, updateProductDto);
+    if (!updateResult.affected) {
+      throw new EntityNotFoundError(Product, id);
+    }
+    return this.productRepository.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  @HttpCode(204)
+  async remove(id: string) {
+    const deleteResult = await this.productRepository.delete(id);
+    if (!deleteResult.affected) {
+      throw new EntityNotFoundError(Product, id);
+    }
   }
 }
